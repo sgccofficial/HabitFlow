00c57c626badb6578d4bc652c3c22bdb7f04b144
sgccofficial <sujithprasanth21@gmail.com>
2026-04-10 19:02:15 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "habit-app-v1";
+
+const urlsToCache = [
+  "index.html",
+  "journal.html",
+  "timer.html"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
