c755d0eac4745c9aebca83114fab3a611dd46cc5
sgccofficial <sujithprasanth21@gmail.com>
2026-04-14 19:29:31 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,6 +10,21 @@ const urlsToCache = [
   "icon-512.png"
 ];
 
+importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  messagingSenderId: "132980995414"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body
+  });
+});
+
 // INSTALL
 self.addEventListener("install", event => {
   self.skipWaiting();
