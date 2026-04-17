19fef8207556fc9c81d6fbcbcb95646b76e4bb73
sgccofficial <sujithprasanth21@gmail.com>
2026-04-17 21:35:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,9 +31,9 @@ app.post("/send", async (req, res) => {
 
     res.send("Notification sent");
   } catch (err) {
-    console.log(err);
-    res.status(500).send("Error");
-  }
+  console.error("FCM ERROR FULL:", err);
+  res.status(500).send(err.message);
+}
 });
 
 const PORT = process.env.PORT || 3000;
